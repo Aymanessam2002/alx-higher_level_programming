1c400acda8e3d32efa688a50cbd5f45472abaaac
Aymanessam2002 <132382531+Aymanessam2002@users.noreply.github.com>
2024-02-07 00:20:45 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
-"""file"""
+"""Script to add arguments to a Python list and save them to a JSON file."""
 import sys
 import json
+from os import path
 
 def save_to_json_file(my_obj, filename):
     with open(filename, 'w') as f:
         json.dump(my_obj, f)
 
 def load_from_json_file(filename):
-    try:
-        with open(filename, 'r') as f:
-            return json.load(f)
-    except (FileNotFoundError, json.JSONDecodeError):
+    if not path.exists(filename):
         return []
+    with open(filename, 'r') as f:
+        return json.load(f)
 
 if __name__ == "__main__":
-    args = ' '.join(sys.argv[1:]).split()
+    args = sys.argv[1:]
     item_list = load_from_json_file("add_item.json")
-
-    if not args:
-        print("Usage: ./7-add_item.py arg1 arg2 ...")
-        sys.exit(1)
-
     item_list.extend(args)
     save_to_json_file(item_list, "add_item.json")
 
-    print("Items added successfully.")
 
 
 
