a11f551dcf9fadd7875a7dfd6b948831be008ed6
Aymanessam2002 <132382531+Aymanessam2002@users.noreply.github.com>
2024-02-07 00:19:29 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,23 +2,20 @@
 """file"""
 import sys
 import json
-from os import path
 
 def save_to_json_file(my_obj, filename):
     with open(filename, 'w') as f:
         json.dump(my_obj, f)
 
 def load_from_json_file(filename):
-    if not path.exists(filename):
-        return []
     try:
         with open(filename, 'r') as f:
             return json.load(f)
-    except json.JSONDecodeError:
+    except (FileNotFoundError, json.JSONDecodeError):
         return []
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
+    args = ' '.join(sys.argv[1:]).split()
     item_list = load_from_json_file("add_item.json")
 
     if not args:
@@ -29,5 +26,6 @@ if __name__ == "__main__":
     save_to_json_file(item_list, "add_item.json")
 
     print("Items added successfully.")
+
 
 
