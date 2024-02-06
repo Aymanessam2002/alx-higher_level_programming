46be2e863a6e9480de77b36abf35aded7af5115b
Aymanessam2002 <132382531+Aymanessam2002@users.noreply.github.com>
2024-02-07 00:17:31 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """file"""
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
 
 if __name__ == "__main__":
     try:
@@ -14,3 +13,4 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     item_list.extend(args)
     save_to_json_file(item_list, "add_item.json")
+
